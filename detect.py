cba3ec8852a56612e4e9b09515167d634457d80f
Sotiris <36128807+SotirisKar@users.noreply.github.com>
2021-10-14 19:36:27 +0300

    Add files via upload

--- detect.py
+++ detect.py
@@ -1,24 +1,22 @@
-import os
-import tensorflow as tf
-from object_detection.utils import label_map_util
 from object_detection.utils import visualization_utils as viz_utils
 from object_detection.builders import model_builder
+from object_detection.utils import label_map_util
 from object_detection.utils import config_util
-import cv2 
+import tensorflow as tf
 import numpy as np
-
+import cv2
+import os
 
 CONFIG_PATH = 'D:/HelmetDetection/ssd-mobilenet-v2-fpnlite/pipeline.config'
 CHECKPOINT_PATH = 'D:/HelmetDetection/training'
 ANNOTATION_PATH = 'D:/HelmetDetection/data'
-
 
 configs = config_util.get_configs_from_pipeline_file(CONFIG_PATH)
 detection_model = model_builder.build(model_config=configs['model'], is_training=False)
 
 # Restore checkpoint
 ckpt = tf.compat.v2.train.Checkpoint(model=detection_model)
-ckpt.restore(os.path.join(CHECKPOINT_PATH, 'ckpt-2')).expect_partial()
+ckpt.restore(os.path.join(CHECKPOINT_PATH, 'ckpt-28')).expect_partial()
 
 @tf.function
 def detect_fn(image):
@@ -30,7 +28,8 @@ def detect_fn(image):
 
 category_index = label_map_util.create_category_index_from_labelmap(ANNOTATION_PATH+'/labelmap.pbtxt')
 
-cap = cv2.VideoCapture(0)
+#cap = cv2.VideoCapture(0)                                           # For Web-Cam
+cap = cv2.VideoCapture('D:/HelmetDetection/Athens_Traffic.mp4')    # For video
 width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
 height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
 
@@ -59,8 +58,8 @@ while True:
                 detections['detection_scores'],
                 category_index,
                 use_normalized_coordinates=True,
-                max_boxes_to_draw=5,
-                min_score_thresh=.5,
+                max_boxes_to_draw=10,
+                min_score_thresh=.7,
                 agnostic_mode=False)
 
     cv2.imshow('object detection',  cv2.resize(image_np_with_detections, (800, 600)))
