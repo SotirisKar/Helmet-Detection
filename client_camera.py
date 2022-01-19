9023651d9b526687df8000f5d4b142fce2136808
Sotiris <36128807+SotirisKar@users.noreply.github.com>
2022-01-19 16:06:01 +0200

    Update client_camera.py

--- client_camera.py
+++ client_camera.py
@@ -13,13 +13,22 @@ import os
 def sendMessage(msg):
     s = socket.socket()        
     port = 8080             
-    s.connect(('192.168.1.79', port))
+    s.connect(('192.168.1.24', port))
     s.send(msg.encode())
     s.close()
 
 class VideoStream:
     def __init__(self,resolution=(640,480),framerate=30):
         self.stream = cv2.VideoCapture(0)
+        for i in range(1,6):
+            if i==5:
+                print('\nNo camera detected\n')
+                os._exit(0)
+            if not self.stream.isOpened():
+                self.stream = cv2.VideoCapture(i)
+                continue
+            else:
+                break
         ret = self.stream.set(cv2.CAP_PROP_FOURCC, cv2.VideoWriter_fourcc(*'MJPG'))
         ret = self.stream.set(3,resolution[0])
         ret = self.stream.set(4,resolution[1])
