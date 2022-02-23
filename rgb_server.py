8977e8a2d4de446b1720000409da6fd875359eac
Sotiris <36128807+SotirisKar@users.noreply.github.com>
2022-02-23 11:43:10 +0200

    Update rgb_server.py

--- rgb_server.py
+++ rgb_server.py
@@ -4,36 +4,37 @@ import socket
 
 s = socket.socket()
 port = 8080
-s.bind(('', port))
+s.bind(("", port))
 s.listen(1)
-print('Socket is listening at port %s' % port)
-image = ''
-prev_msg = ''
+print(f"Socket is listening at port {port}")
+image = ""
+prev_msg = ""
 n=h=nh = Process(target=displayMatrix, args=([image]))
 
 while True:
     c, addr = s.accept()
-    c.send('Connection successful'.encode())
-    if 'msg' in locals():   # Keep memory of previous message
+    print(f"Connection established with {addr}")
+    c.send("Connection established".encode())
+    if "msg" in locals():                                            # keep memory of previous message
         prev_msg = msg
     msg = c.recv(2).decode()
     c.close()
-    if prev_msg != msg:     # To avoid flickering & 2 images same time 
-        if msg == 'H':
-            image = '/home/pi/helmet-detection/images/helmet.jpg' # full path required
+    if prev_msg != msg:                                              # to avoid flickering & 2 images at the same time 
+        if msg == "H":
+            image = "/home/pi/helmet-detection/images/helmet.jpg"    # full path required
             h = Process(target=displayMatrix, args=([image]))
             if nh.is_alive() == True:
                 nh.terminate()
                 h.start()
 
-        elif msg == 'NH':
-            image = '/home/pi/helmet-detection/images/no_helmet.jpg' # full path required
+        elif msg == "NH":
+            image = "/home/pi/helmet-detection/images/no_helmet.jpg" # full path required
             nh = Process(target=displayMatrix, args=([image]))
             if h.is_alive() == True:
                 h.terminate()
             nh.start()
 
-        elif msg == 'N':
+        elif msg == "N":                                             # stop displaying the image
             if h.is_alive() == True:
                 h.terminate()
             if nh.is_alive() == True:
