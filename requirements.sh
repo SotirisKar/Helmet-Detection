2216e842f906771dfac6b52c684f6ecfe6d8203a
Sotiris <36128807+SotirisKar@users.noreply.github.com>
2022-03-04 11:22:14 +0200

    Add files via upload

--- requirements.sh
+++ requirements.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
+#bash requirements.sh
 
-# Run "bash requirements.sh"
 sudo apt install python3-dev python3-pip
 pip3 install opencv-python
+pip3 install APScheduler
+pip3 install PyDrive
+pip3 install pytz
+pip3 install pandas
+pip3 install numpy
 pip3 install https://github.com/google-coral/pycoral/releases/download/v2.0.0/tflite_runtime-2.5.0.post1-cp37-cp37m-linux_armv7l.whl
-echo Installation completed
+
+echo "INFO: Installation Complete."
