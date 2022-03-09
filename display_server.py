9b34e2a0c7794366b9adecf20b39e144bc6b7be3
Sotiris <36128807+SotirisKar@users.noreply.github.com>
2022-03-09 11:46:25 +0200

    Update display_server.py

--- display_server.py
+++ display_server.py
@@ -1,7 +1,7 @@
 from utils.display import displayMatrix
 from multiprocessing import Process
 import socket
-
+import time
 
 os.chdir(os.path.dirname(os.path.abspath(__file__))
 s = socket.socket()
@@ -11,8 +11,7 @@ s.listen(1)
 print(f"Socket is listening at port {port}")
 image = ""
 prev_msg = ""
-n=h=nh = Process(target=displayMatrix, args=([image]))
-
+n=h=nh=con = Process(target=displayMatrix, args=([image]))
 while True:
     c, addr = s.accept()
     print(f"Connection established with {addr}")
@@ -23,20 +22,20 @@ while True:
     c.close()
     if prev_msg != msg:                                              # to avoid flickering & 2 images at the same time 
         if msg == "H":
-            image = "images/helmet.jpg"    # full path required
+            image = "images/helmet.jpg"
             h = Process(target=displayMatrix, args=([image]))
             if nh.is_alive() == True:
                 nh.terminate()
                 h.start()
 
         elif msg == "NH":
-            image = "images/no_helmet.jpg" # full path required
+            image = "images/no_helmet.jpg"
             nh = Process(target=displayMatrix, args=([image]))
             if h.is_alive() == True:
                 h.terminate()
             nh.start()
 
-        elif msg == "N":                                             # stop displaying the image
+        elif msg == "N":                                              # stop displaying the image
             if h.is_alive() == True:
                 h.terminate()
             if nh.is_alive() == True:
