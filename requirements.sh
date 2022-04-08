4dd59f4649a7bb77d22f777a582823ef098fea58
Sotiris <36128807+SotirisKar@users.noreply.github.com>
2022-04-08 12:16:19 +0300

    Update requirements.sh

--- requirements.sh
+++ requirements.sh
@@ -10,9 +10,10 @@ python3 -m pip install python-periphery
 python3 -m pip install APScheduler
 python3 -m pip install screeninfo
 python3 -m pip install PyDrive
-python3 -m pip install pytz
 python3 -m pip install pandas
+python3 -m pip install Flask
 python3 -m pip install numpy
+python3 -m pip install pytz
 python3 -m pip install https://github.com/google-coral/pycoral/releases/download/v2.0.0/tflite_runtime-2.5.0.post1-cp37-cp37m-linux_armv7l.whl
 
 echo "INFO: Installation Complete."
