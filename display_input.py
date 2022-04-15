2894e75e2972d85433858badada9a8a515da0e20
Sotiris <36128807+SotirisKar@users.noreply.github.com>
2022-04-15 10:31:20 +0300

    Update display_input.py

--- display_input.py
+++ display_input.py
@@ -8,21 +8,20 @@ image = "images/connected.jpg"
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
 n=h=b=nh = Process(target=displayMatrix, args=[image])
 
-GPIO.setwarnings(False)                                     # Disable Warnings
+GPIO.setwarnings(False)                                     # Disable GPIO Warnings
 GPIO.setmode(GPIO.BCM)                                      # Read GPIOs
 GPIO.setup(18, GPIO.IN)
 GPIO.setup(24, GPIO.IN)
 prev_gpio = [GPIO.input(18),GPIO.input(24)]                 # Remember previous state for GPIOs
 print("INFO: Starting GPIO input detection.")
 
-n.start()                                                   # Visual Confirmation that script runs
+n.start()                                                   # Visual confirmation that script runs
 time.sleep(3)
 n.terminate()
 
-# To Get Rid of zombie colored pixels
 image = "images/black.jpg"
 b = Process(target=displayMatrix, args=[image])
-b.start()
+b.start()                                                   # Get rid of zombie colored pixels
 time.sleep(.5)
 b.terminate()
 
@@ -34,21 +33,21 @@ while True:
             image = "images/helmet.jpg"                     # Display helmet image
             h = Process(target=displayMatrix, args=[image])
             if nh.is_alive() == True:
-                nh.terminate()                              # Kill previous image
+                nh.terminate()                              # Terminate previous image to display new
             h.start()
-        elif prev_gpio == [1,0]:                            # Display No Helmet image
+        elif prev_gpio == [1,0]:                            # Display no helmet image
             image = "images/no_helmet.jpg"
             nh = Process(target=displayMatrix, args=[image])
             if h.is_alive() == True:
                 h.terminate()
             nh.start()
-        elif prev_gpio == [0,0]:                            # Stop Displaying image
+        elif prev_gpio == [0,0]:                            # Stop displaying any image
             if h.is_alive() == True:
                 h.terminate()
             if nh.is_alive() == True:
                 nh.terminate()
 
             b = Process(target=displayMatrix, args=[image])
-            b.start()
+            b.start()                                       # Get rid of zombie colored pixels
             time.sleep(.5)
             b.terminate()
